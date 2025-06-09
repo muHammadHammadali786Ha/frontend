75104bcf74dca710f42bc548b8291031ee93415b
muHammadHammadali786Ha <sunm25377@gmail.com>
2025-06-09 10:35:50 +0500

    addFix

---
+++ remix.config.js
@@ -0,0 +1,6 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export const appDirectory = "app";
+export const assetsBuildDirectory = "dist/client";
+export const publicPath = "/build/";
+export const serverBuildPath = "dist/server/index.js";
+export const serverModuleFormat = "cjs";
